b8e2d46bfebd3e87b27112d05d5824c9a73c93b8
Michael Klein <michael@lttl.dev>
2026-04-19 18:55:14 +0200

    add missing FIXME

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Fprintln(cmd.OutOrStdout(), "0.1.0")
+		fmt.Fprintln(cmd.OutOrStdout(), "v0.1.0") // FIXME: make this dynamic based on the actual version
 	},
 }
 
